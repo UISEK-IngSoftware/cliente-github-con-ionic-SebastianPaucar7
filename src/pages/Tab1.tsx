c0d0750a2259c8231a68cf4001a27f7b92034988
SebastianPaucar7 <sebastian.paucar@uisek.edu.ec>
2025-12-03 11:41:11 -0500

    Mensaje

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,13 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import RepoItem from '../components/RepoItem';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Lista de repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +17,20 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          <RepoItem
+            name="android-project"
+            imageUrl="https://www.liderlogo.es/wp-content/uploads/2022/12/Logo-Android-1024x640.png"
+          />
+          <RepoItem
+            name="ios-project"
+            imageUrl="https://cdn.vectorstock.com/i/1000v/21/41/ios-icon-logo-software-phone-apple-symbol-design-vector-46322141.jpg"
+          />
+          <RepoItem
+            name="ionic-project"
+            imageUrl="https://images-cdn.openxcell.com/wp-content/uploads/2024/07/25072610/ionic-inner.svg"
+          />  
+        </IonList>
       </IonContent>
     </IonPage>
   );
