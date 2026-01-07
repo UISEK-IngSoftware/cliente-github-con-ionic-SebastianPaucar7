82f12e86384a99924949ae2b57818dfcb88afa3b
SebastianPaucar7 <sebastian.paucar@uisek.edu.ec>
2026-01-07 10:58:21 -0500

    Laboratorio 10 Completado

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -1,16 +1,27 @@
 import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItem";
 import { UserInfo } from "../interfaces/UserInfo";
+import AuthService from "./AuthService";
 
 const GITHUB_API_URL = import.meta.env.VITE_API_URL;
-const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN;
+
+const githubApi = axios.create({
+    baseURL: GITHUB_API_URL,
+});
+
+githubApi.interceptors.request.use((config) => {
+    const AuthHeader = AuthService.getAuthHeader();
+    if (AuthHeader) {
+        config.headers.Authorization = AuthHeader;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
     try {
-        const response = await axios.get(`${GITHUB_API_URL}/user/repos` , {
-            headers: {
-                Authorization: `Bearer ${GITHUB_API_TOKEN}`,
-            },
+        const response = await githubApi.get(`/user/repos` , {
             params: {
                 per_page: 100,
                 sort: "created",
@@ -37,34 +48,26 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
 
 export const createRepository = async (repo: RepositoryItem): Promise<void> => {
     try {
-        const reponse = await axios.post(`${GITHUB_API_URL}/user/repos`, repo,{
-            headers: {
-                Authorization: `Bearer ${GITHUB_API_TOKEN}`,
-            }
-        });
-        console.log("Repositorio creado:", reponse.data);
-
+        const response = await githubApi.post(`/user/repos`, repo)
+        console.log("Repositorio creado", response.data);
+        
     } catch (error) {
-        console.error("Error al crear repositorio", error);
+        console.error("Ocurrió un error al crear el repositorio:", error);
     }
 };
 
-export const getUserInfo = async (): Promise<UserInfo | null> => {
+export const getUserInfo = async (): Promise<UserInfo> => {
     try {
-        const response = await axios.get(`${GITHUB_API_URL}/user`, {
-            headers: {
-                Authorization: `Bearer ${GITHUB_API_TOKEN}`,
-            }
-        });
+        const response = await githubApi.get(`/user`);
         return response.data as UserInfo;
-   } catch (error) {
-    console.error("Error al obtener información del usuario:", error);
-    const userNotFound :UserInfo = {
-        login: 'underfined',
-        name: 'Usuario no encontrado',
-        bio: 'No se pudo obtener la información del usuario.',
-        avatar_url: 'https://www.svgrepo.com/show/77036/user.svg',
+    } catch (error) {
+        console.error("Ocurrió un error al obtener la información del usuario:", error);
+        const userNotFound : UserInfo = {
+            login: "undefined",
+            name: "Usuario no encontrado",
+            bio: "No se pudo obtener la información del usuario.",
+            avatar_url: 'https://www.svgrepo.com/show/77036/user.svg',
+        }
+        return userNotFound;
     }
-    return userNotFound;
- }
 };
